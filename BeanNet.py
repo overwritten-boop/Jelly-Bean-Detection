dc5b31b4f8a48c7ac476e5aa8edca87f9bd7e392
overwritten-boop <overwritten.mail@gmail.com>
2022-08-05 21:04:08 -0400

    1.0
    version 1 of the python script pre bugfixing

---
+++ BeanNet.py
@@ -0,0 +1,34 @@
+#imports libraries to be used later in the program
+import jetson.inference
+import jetson.utils
+
+import argparse
+
+#creates a parser object to translate the command line to variables
+parser = argparse.ArgumentParser()
+
+#adds the arguments to the cmd command
+#allows for arguments given in the command line to be taken as variables
+parser.add_argument("filename", type=str, help="filename of the image to process")
+parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be:  googlenet, resnet-18, ect. (see --help for others)")
+parser.add_argument("flavor", type=str, help="Type of jelly bean to be detected")
+
+#creates a parser object that allows the command line variables to be passed into the program
+opt = parser.parse_args()
+
+#loads the image passed in the parameters
+img = jetson.utils.loadImage(opt.filename)
+
+#gets the network from the file path provided in the parameters
+net = jetson.inference.imageNet(opt.network)
+
+while True:
+    #begins the classification network process
+    #assigns the class that is most probable as class_idx(str), and the confidience as confidence(int)
+    class_idx, confidence = net.Classify(img)
+
+
+    class_desc = net.GetClassDesc(class_idx)
+    if(class_desc == opt.flavor):
+        #here any result from detecting the bean flavor
+        print("WARNING >>> YUCKY BEAN DETECTED")
