f87b8665567d09f69c7ca07e71ed9da6e016c218
overwritten-boop <overwritten.mail@gmail.com>
2022-08-07 21:34:07 -0400

    Update To BeanNet.py 1.11
    adjusts the args of the jetson.inference.imagenet in hopes of success

--- BeanNet.py
+++ BeanNet.py
@@ -22,7 +22,7 @@ opt = parser.parse_args()
 input = jetson.utils.videoSource("/dev/video0")
 
 #gets the network from the file path provided in the parameters
-net = jetson.inference.imageNet("--network=resnet-18", argv = ["--model=beansnet18.onnx", "--labels=labels.txt"])
+net = jetson.inference.imageNet("resnet-18", argv = ["--model=beansnet18.onnx", "--labels=labels.txt"])
 
 while True:
     #gets the input from the
